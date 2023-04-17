ffc97502981b3b90e321664b934faae835c9b1fd
Harze2k <53750202+Harze2k@users.noreply.github.com>
2023-04-17 17:32:15 +0200

    Update Get-RegInfoByLastWriteTime.ps1
    1.1 Added description.

--- Get-RegInfoByLastWriteTime/Get-RegInfoByLastWriteTime.ps1
+++ Get-RegInfoByLastWriteTime/Get-RegInfoByLastWriteTime.ps1
@@ -1,3 +1,27 @@
+<#
+Description: This C# code snippet provides a set of utility functions for querying Windows registry key information and converting file time to system time. The code leverages Platform Invoke (P/Invoke) to call native Win32 API functions from the advapi32.dll and kernel32.dll libraries.
+Win32API Class
+The Win32API class contains the following members:
+RegQueryInfoKey Function
+This static function is a wrapper around the native RegQueryInfoKey function from the advapi32.dll library. It retrieves information about a specified registry key, such as the number of subkeys, values, and the last write time. The function accepts the following parameters:
+    hkey: A handle to the registry key.
+    lpClass: A pointer to a buffer that receives the key class.
+    lpcbClass: A pointer to a variable that specifies the size of the buffer pointed to by the lpClass parameter, in characters.
+    lpReserved: Reserved; must be IntPtr.Zero.
+    lpcSubKeys: A pointer to a variable that receives the number of subkeys that the key contains.
+    lpcbMaxSubKeyLen: A pointer to a variable that receives the size of the key's longest subkey name, in characters.
+    lpcbMaxClassLen: A pointer to a variable that receives the size of the longest string that specifies a subkey's class, in characters.
+    lpcValues: A pointer to a variable that receives the number of values that are associated with the key.
+    lpcbMaxValueNameLen: A pointer to a variable that receives the size of the key's longest value name, in characters.
+    lpcbMaxValueLen: A pointer to a variable that receives the size of the key's longest value data, in bytes.
+    lpcbSecurityDescriptor: A pointer to a variable that receives the size of the key's security descriptor, in bytes.
+    lpftLastWriteTime: A pointer to a variable that receives the last write time.
+FileTimeToSystemTime Function
+This static function is a wrapper around the native FileTimeToSystemTime function from the kernel32.dll library. It converts a file time to system time format. The function accepts the following parameters:
+lpFileTime: A pointer to a long variable containing the file time to be converted.
+lpSystemTime: A pointer to a SYSTEMTIME structure that receives the converted system time.
+SYSTEMTIME Structure
+#>
 Add-Type @'
     using System;
     using System.Text;
@@ -27,6 +51,23 @@ Add-Type @'
         }
     }
 '@
+<##
+This PowerShell function, Get-RegInfoByLastWriteTime, retrieves information about installed software on a Windows system by querying the registry keys associated with installed software. The function primarily focuses on obtaining the software's last write time, but it also collects other relevant information, such as display name, display version, uninstall strings, install location, and more.
+Function Parameters:
+[Optional] $FullPaths: A string array containing custom registry paths to be queried. If not provided, default registry paths for uninstall information will be used.
+Function Process:
+    1. Retrieves the current user's username and creates a new NTAccount object for the user.
+    2. Defines default registry paths for the software uninstall keys in the user and system hives.
+    3. Replaces any registry path abbreviations with their full paths and separates the hive and path for each registry key.
+    4. Iterates through the registry keys, opening the corresponding subkeys and extracting their subkey names.
+    5. For each subkey, creates a custom PowerShell object containing the subkey name, path, key, and hive information.
+    6. Iterates through the subkeys and opens the corresponding registry key based on the key property (Users-Default, LocalMachine-Registry64, or LocalMachine-Registry32).
+    7. Queries the registry key for information using the Win32API RegQueryInfoKey method, and retrieves the last write time of the key.
+    8. Retrieves additional information from the subkey, such as display name, uninstall strings, and more.
+    9. If the last write time and display name are valid and unique, adds a custom PowerShell object containing the retrieved information to the results list.
+    10. Returns the results list, sorted by the last write time in descending order.
+The output of this function is a sorted list of PowerShell custom objects containing information about installed software on the system, primarily focusing on the software's last write time but including other relevant details as well.
+#>
 function Get-RegInfoByLastWriteTime {
     [CmdletBinding()] 
     param (
